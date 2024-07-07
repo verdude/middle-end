6a75e8d490c43b8b2de83169545a191632075fdb
Santiago Verdu <erra@qmake.org>
2024-07-07 14:59:31 -0600

    dinner

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const ProtoParser = @import("./proto.zig");
 
 const os = std.os;
 
@@ -25,12 +26,30 @@ fn middle() !void {
     var con1: ?std.net.Server.Connection = null;
     while (true) {
         const con = try server.accept();
+
         if (con1) |c1| {
             std.log.info("第二連線{?}", .{con});
-            std.log.info("傳送到二...", .{});
-            try c1.address.format("", .{}, con.stream.writer());
-            std.log.info("傳送到一...", .{});
+            std.log.info("傳送到二：自己的數字：「{d}」", .{con.address.in.getPort()});
+            try ProtoParser.writePeer(
+                c1.address,
+                con.stream.writer(),
+            );
+            try ProtoParser.writePort(
+                con.address.in.getPort(),
+                con.stream.writer(),
+            );
+            defer {
+                c1.stream.close();
+                con.stream.close();
+            }
+
+            std.log.info("傳送到一: 自己的數字：「{d}」", .{c1.address.in.getPort()});
             try con.address.format("", .{}, c1.stream.writer());
+            try ProtoParser.writePeer(
+                con.address,
+                c1.stream.writer(),
+            );
+
             std.log.info("完成了.", .{});
             break;
         } else {
@@ -38,6 +57,23 @@ fn middle() !void {
             std.log.info("第一連線{?}", .{con1});
         }
     }
+}
+
+fn client(address: std.net.Address) !void {
+    var stream = try std.net.tcpConnectToAddress(address);
+    var buf = [1]u8{0} ** 24;
+    if (try stream.reader().read(&buf) == 0) {
+        std.log.err("Connection closed...", .{});
+        return error{ProxyConnectionClosed}.ProxyConnectionClosed;
+    }
+    var response = ProtoParser.init(&buf);
+    try response.parse();
+    std.log.debug("Got ip4: {?}", .{response.peer});
+    if (try response.isServer()) {
+        //const server = bindPort(ip4.getPort());
+    }
+
+    try tryConnect(response.peer.?);
 }
 
 fn waitForMessage(stream: std.net.Stream) !void {
@@ -59,35 +95,15 @@ fn bindPort(port: u16) !std.net.Server {
     return try addr.listen(.{ .reuse_address = true });
 }
 
-fn addressFromString(buf: []const u8) !std.net.Address {
-    var i: usize = 0;
-    while (i < buf.len and buf[i] != ':') : (i += 1) {}
-    var j = i;
-    while (!std.ascii.isDigit(buf[j])) : (j += 1) {}
-    const port_start = j;
-    while (std.ascii.isDigit(buf[j])) : (j += 1) {}
-    const port_end = port_start + j - port_start;
-    std.log.debug("Address comp: {s} {s}", .{ buf[0..i], buf[port_start..port_end] });
-    const port = try std.fmt.parseInt(u16, buf[port_start..port_end], 10);
-    return try std.net.Address.parseIp4(buf[0..i], port);
-}
-
-fn client(address: std.net.Address) !void {
-    var stream = try std.net.tcpConnectToAddress(address);
-    var buf = [1]u8{0} ** 24;
-    if (try stream.reader().read(&buf) == 0) {
-        std.log.err("Connection closed...", .{});
-        return error{ProxyConnectionClosed}.ProxyConnectionClosed;
+fn tryConnect(address: std.net.Address) !void {
+    const peer: ?std.net.Stream = std.net.tcpConnectToAddress(address) catch null;
+    if (peer) |con| {
+        if (try con.writer().write("你好，我愛你") == 0) {
+            std.log.err("大過", .{});
+            return error{cuole}.cuole;
+        }
+        try waitForMessage(con);
     }
-    const ip4 = try addressFromString(&buf);
-    std.log.debug("Got ip4: {?}", .{ip4});
-
-    var peer = try std.net.tcpConnectToAddress(address);
-    if (try peer.writer().write("你好我愛你") == 0) {
-        std.log.err("大過", .{});
-        return error{cuole}.cuole;
-    }
-    try waitForMessage(peer);
 }
 
 pub fn main() !void {
